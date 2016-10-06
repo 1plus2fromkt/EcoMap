8dcb2fd5369bd95384ec76dc643517f6ab109379
nikitaevg <vorobyev.nikita@gmail.com>
2016-10-06 23:33:45 +0300

    Added location button, categories checking and search edit

--- app/src/main/java/com/twofromkt/ecomap/MapActivity.java
+++ app/src/main/java/com/twofromkt/ecomap/MapActivity.java
@@ -2,13 +2,20 @@ package com.twofromkt.ecomap;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
@@ -22,19 +29,25 @@ import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Arrays;
 
+import static com.twofromkt.ecomap.CategoriesActivity.CHOSEN_KEY;
+import static com.twofromkt.ecomap.CategoriesActivity.TRASH_N;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback, FloatingActionButton.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
-    private GoogleMap mMap;
+    GoogleMap mMap;
     MapView mapView;
-    private FloatingActionButton cafeButton, trashButton;
-    private CameraPosition startPos;
-    private FloatingActionMenu floatingMenu;
+    FloatingActionButton cafeButton, trashButton, locationButton;
+    CameraPosition startPos;
+    FloatingActionMenu floatingMenu;
     SupportMapFragment mapFragment;
+    EditText search_edit;
     Fragment fragment;
     boolean[] chosen;
     NavigationView nv;
+    Criteria criteria = new Criteria();
+    LocationManager locationManager;
 
-    static final String MENU_OPENED = "MENU_OPENED", LAT = "LAT", LNG = "LNG", ZOOM = "ZOOM";
+    static final String MENU_OPENED = "MENU_OPENED", LAT = "LAT", LNG = "LNG", ZOOM = "ZOOM", SEARCH_TEXT="SEARCH_TEXT";
     static final int CHOOSE_TRASH_ACTIVITY = 0;
 
     @Override
@@ -45,13 +58,22 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         cafeButton = (FloatingActionButton) findViewById(R.id.cafe_button);
+        search_edit = (EditText) findViewById(R.id.search_edit);
+
+        search_edit.setCursorVisible(false);
+        if (savedInstanceState == null)
+            search_edit.setHint("Search query");
         trashButton = (FloatingActionButton) findViewById(R.id.trash_button);
         floatingMenu = (FloatingActionMenu) findViewById(R.id.menu);
+        if (chosen == null)
+            chosen = new boolean[TRASH_N];
+        locationButton = (FloatingActionButton) findViewById(R.id.location_button);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         fragment = getFragmentManager().findFragmentById(R.id.map);
         if (savedInstanceState != null) {
             startPos = CameraPosition.fromLatLngZoom(new LatLng(
-                    (double) savedInstanceState.get(LAT),
-                    (double) savedInstanceState.get(LNG)),
+                            (double) savedInstanceState.get(LAT),
+                            (double) savedInstanceState.get(LNG)),
                     (float) savedInstanceState.get(ZOOM));
             if (savedInstanceState.getBoolean(MENU_OPENED)) {
                 floatingMenu.open(false);
@@ -59,8 +81,10 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         }
         mapFragment.getMapAsync(this);
         trashButton.setOnClickListener(this);
+        locationButton.setOnClickListener(this);
         nv = (NavigationView) findViewById(R.id.nav_view);
         nv.setNavigationItemSelectedListener(this);
+
     }
 
     /**
@@ -76,7 +100,19 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         if (startPos != null)
-            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(startPos));
+            moveMap(mMap, startPos);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
 //        LatLng sydney = new LatLng(-34, 151);
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
@@ -86,28 +122,63 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     public void onClick(View v) {
         if (v == trashButton) {
             Intent intent = new Intent(getApplicationContext(), CategoriesActivity.class);
+            intent.putExtra(CHOSEN_KEY, chosen);
             startActivityForResult(intent, CHOOSE_TRASH_ACTIVITY);
         }
+        if (v == locationButton) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Location location = locationManager.getLastKnownLocation(locationManager
+                    .getBestProvider(criteria, false));
+            if (location != null)
+                moveMap(mMap, fromLatLng(location.getLatitude(), location.getLongitude(), 10));
+        }
+    }
+
+    private CameraPosition fromLatLng(double a, double b, float z) {
+        return CameraPosition.fromLatLngZoom(new LatLng(a, b), z);
+    }
+
+    private void moveMap(GoogleMap map, CameraPosition pos) {
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(pos));
     }
 
     public void onSaveInstanceState(Bundle state) {
         super.onSaveInstanceState(state);
         state.putBoolean(MENU_OPENED, floatingMenu.isOpened());
-        LatLng ll = mMap.getCameraPosition().target;
-        state.putDouble(LAT, ll.latitude);
-        state.putDouble(LNG, ll.longitude);
-        state.putFloat(ZOOM, mMap.getCameraPosition().zoom);
+        state.putBooleanArray(CHOSEN_KEY, chosen);
+        if (mMap != null) { // mMap can be null if we turn phone just after onCreate
+            LatLng ll = mMap.getCameraPosition().target;
+            state.putDouble(LAT, ll.latitude);
+            state.putDouble(LNG, ll.longitude);
+            state.putFloat(ZOOM, mMap.getCameraPosition().zoom);
+        }
+        state.putCharSequence(CHOSEN_KEY, search_edit.getText());
     }
 
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         if (savedInstanceState != null) {
-            startPos = CameraPosition.fromLatLngZoom(new LatLng(
-                            (double) savedInstanceState.get(LAT),
-                            (double) savedInstanceState.get(LNG)),
-                    (float) savedInstanceState.get(ZOOM));
+            try {
+                startPos = CameraPosition.fromLatLngZoom(new LatLng(
+                                (double) savedInstanceState.get(LAT),
+                                (double) savedInstanceState.get(LNG)),
+                        (float) savedInstanceState.get(ZOOM));
+            } catch (Exception ignored) {
+
+            }
             if (savedInstanceState.getBoolean(MENU_OPENED)) {
                 floatingMenu.open(false);
             }
+            chosen = savedInstanceState.getBooleanArray(CHOSEN_KEY);
+            search_edit.setText(savedInstanceState.getCharSequence(SEARCH_TEXT));
         }
     }
 
